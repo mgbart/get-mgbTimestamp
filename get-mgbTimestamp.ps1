8e329fc83be5fd89fbeed3440c55abf3185ec410
Matias Bernhardt <matias.bernhardt@gmail.com>
2021-12-02 16:29:45 -0300

    Initial commit via upload

---
+++ get-mgbTimestamp.ps1
@@ -0,0 +1,26 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.188
+	 Created on:   	12/2/2021 12:59 PM
+	 Created by:   	Matías Bernhardt
+	 Organization: 	MGB
+	 Filename:     	get-mgbTimestamp
+	===========================================================================
+	.DESCRIPTION
+		Grabs format from XML file.
+		Fills the clipboard with a Timestamp. 
+		Set a keyboard shortcut to easily have a timestamp always at hand.
+#>
+
+try {
+	$xml = [xml](Get-Content $PSScriptRoot\config.xml -ErrorAction Stop)
+	$format = $xml.Config.Format
+}
+catch {
+	$format = "dd/MM/yyyy | hh:mm"	
+}
+
+
+
+Get-Date -Format "$format" | Set-Clipboard
